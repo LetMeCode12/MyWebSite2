1cd9da53faa147c3ab517940d98fbdf1b80d3380
LetMeCode12 <you@example.com>
2021-03-24 14:01:34 +0100

    add

--- src/Components/myInput/myInput.js
+++ src/Components/myInput/myInput.js
@@ -7,14 +7,14 @@ export default function MyInput (props) {
     const errorRef = useRef();
 
     const isError=()=>touched[field.name]&&errors[field.name];
-    useEffect(()=>{
-        if(isError){
-            errorRef.current.setAttribute("ErrorText",errors[field.name])
-        }
-    })
+    // useEffect(()=>{
+    //     if(isError){
+    //         errorRef.current.setAttribute("ErrorText",errors[field.name])
+    //     }
+    // })
     
     return(
-        <div className={`MyInput ${isError()?"Error":""}`} ref={errorRef}>
+        <div className={`MyInput ${isError()?"Error":""}`} ErrorText={errors[field.name]} ref={errorRef}>
             <input className={`form-control ${isError()?"Error":""}`} {...field} {...props}/>
         </div>
     )
